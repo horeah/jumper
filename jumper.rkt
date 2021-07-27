0f3aba01eeab3520a59bc550aac670049bb23356
Horea Haitonic <horea.haitonic@qtronic-software.ro>
2021-07-27 18:56:16 +0300

    Limit length of results list
    The list of matches is now limited to MAX-ENTRIES; the limit can be
    increased by the user selecting the SHOW-MORE entry.

--- jumper.rkt
+++ jumper.rkt
@@ -1,4 +1,9 @@
 #lang racket/gui
+(current-directory (find-system-path 'home-dir))
+
+(define (debug text)
+  (writeln text)
+  (flush-output))
 
 (define frame (new frame%
                    [label "Jumper"]
@@ -6,6 +11,8 @@
                    [min-height 400]))
 
 (define all-files (list))
+(define MAX-ENTRIES 100)
+(define SHOW-MORE "<show more entries>")
 
 (define (path->entry path)
   (define path-string (path->string path))
@@ -15,7 +22,9 @@
   (define updated-entries
     (filter (lambda (filename) (string-contains? filename filter-value))
             (map path->entry all-files)))
-  (send entries set updated-entries)
+  (send entries set (if (< (length updated-entries) MAX-ENTRIES)
+                           updated-entries
+                           (append (take updated-entries MAX-ENTRIES) (list SHOW-MORE))))
   (with-handlers ([exn:fail:contract? (lambda (exn) null)])
     (send entries select 0)))
 
@@ -28,9 +37,14 @@
                    (case (send event get-event-type)
                      ['text-field-enter
                       (begin
-                        (system (string-join (list "explorer"
-                                                   (send entries get-string (send entries get-selection)))))
-                        (exit))]
+                        (define selection (send entries get-string (send entries get-selection)))
+                        (if (equal? selection SHOW-MORE)
+                            (begin
+                              (set! MAX-ENTRIES (* 2 MAX-ENTRIES))
+                              (update-list))
+                            (begin
+                              (system (string-join (list "explorer" selection)))
+                              (exit))))]
                      ['text-field
                       (begin
                         (set! filter-value (send filter-text get-value))
@@ -39,9 +53,10 @@
 
 (define entries (new list-box%
                      [label #f]
-                     [choices '("initial")]
+                     [choices (list "")]  ; empty list not accepted; we clear later
                      [selection 0]
                      [parent frame]))
+(send entries clear)
 
 (send frame show #t)
 
@@ -50,7 +65,11 @@
           (fold-files
            (lambda (path type result)
              (if (string-contains? (path->string path) filter-value)
-                 (send entries append (path->entry path))
+                 (cond
+                   [(< (send entries get-number) MAX-ENTRIES)
+                    (send entries append (path->entry path))]
+                   [(= (send entries get-number) MAX-ENTRIES)
+                    (send entries append SHOW-MORE)])
                  null)
              (set! all-files (append all-files (list path)))
              (set! access #t)
@@ -58,6 +77,6 @@
                  (with-handlers ([exn? (lambda (exn) (set! access #f))])
                    (directory-list path))
                  null)
-             (values null access))
+             (values null access))  ; only mark for traversal if we were able to get access
            (list))
-          (writeln "Done loading")))
+          (debug "Done loading")))
